2f0bc4ff4876cc5c2e4b10984a8d58dc39af3c07
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-27 17:43:53 +0300

    GroceryList.java

---
+++ src/com/workintech/shoppingcart/GroceryList.java
@@ -0,0 +1,48 @@
+package com.workintech.shoppingcart;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class GroceryList {
+    private ArrayList<String> items;
+
+    public GroceryList(ArrayList<String> items) {
+        this.items = items;
+    }
+
+    public void addItems(String items) {
+        if (items.contains(",")) {
+            String[] splitItems = items.split(",");
+            for (String item : splitItems) {
+                String trimmedItem = item.trim();
+                if (checkItemIsInList(trimmedItem)) System.out.println("Item " + item + " is already in the list.");
+                else this.items.add(trimmedItem);
+                printSorted();
+            }
+        }
+        else {
+            this.items.add(items);
+        }
+    }
+
+    public void removeItems(String items) {
+        String[] splitItems = items.split(",");
+        for(String item: splitItems) {
+            String trimmedItem = item.trim();
+            if (checkItemIsInList(trimmedItem)) this.items.remove(trimmedItem);
+            else System.out.println("Item " + item + " is already in the list.");
+        }
+    }
+
+    public boolean checkItemIsInList(String item) {
+        return items.contains(item);
+    }
+
+    public void printSorted() {
+        Collections.sort(this.items);
+        System.out.println("Current grocery list:");
+        for(String item:this.items) {
+            System.out.println(item);
+        }
+    }
+}
