4426acfafad8edc8c0305f01f2bbd882b56b4bc9
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-27 15:01:11 +0300

    Scanner. if else structure for the value scanned

---
+++ src/com/workintech/shoppingcart/Main.java
@@ -0,0 +1,24 @@
+package com.workintech.shoppingcart;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("0 for exit, 1 for adding products, 2 for removing products: ");
+        int number = scanner.nextInt();
+
+        if (number == 0) {
+
+        }
+        else if (number == 1) {
+
+        }
+        else if (number == 2) {
+
+        }
+        else {
+            System.out.println("Invalid number or character. Please enter 0, 1, or 2 only.");
+        }
+    }
+}
