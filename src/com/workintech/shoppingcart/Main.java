d500f25a119a46b1735954801185951ad1a77ed3
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-27 17:58:16 +0300

    Multiple scanner was redundant. Added to the first line in do while

--- src/com/workintech/shoppingcart/Main.java
+++ src/com/workintech/shoppingcart/Main.java
@@ -8,24 +8,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("0 for exit, 1 for adding products, 2 for removing products: ");
-        int number = scanner.nextInt();
-
+        int number;
         GroceryList groceryList = new GroceryList();
 
         do {
+            number = scanner.nextInt();
             if (number == 1) {
                 System.out.println("List products you want to add to the list separating with commas: ");
                 String products = scanner.nextLine();
                 groceryList.addItems(products);
-                number = scanner.nextInt();
             } else if (number == 2) {
                 System.out.println("List products you want to remove from the list separating with commas: ");
                 String products = scanner.nextLine();
                 groceryList.removeItems(products);
-                number = scanner.nextInt();
             } else {
                 System.out.println("Invalid number or character. Please enter 0, 1, or 2 only.");
-                number = scanner.nextInt();
             }
             groceryList.printSorted();
         }
