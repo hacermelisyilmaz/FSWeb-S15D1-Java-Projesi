6922f6b2e5f3a6754e139f7047eb63652d2013ce
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-27 15:56:55 +0300

    addItems() method is created

--- src/com/workintech/shoppingcart/Main.java
+++ src/com/workintech/shoppingcart/Main.java
@@ -1,5 +1,7 @@
 package com.workintech.shoppingcart;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,17 +10,33 @@ public class Main {
         System.out.println("0 for exit, 1 for adding products, 2 for removing products: ");
         int number = scanner.nextInt();
 
+        List<String> groceryList = new ArrayList<>();
+
         if (number == 0) {
 
+            number = scanner.nextInt();
         }
         else if (number == 1) {
-
+            number = scanner.nextInt();
         }
         else if (number == 2) {
-
+            number = scanner.nextInt();
         }
         else {
             System.out.println("Invalid number or character. Please enter 0, 1, or 2 only.");
         }
+    }
+
+    public static ArrayList<String> addItems(ArrayList<String> groceryList, String itemStr) {
+        if (itemStr.contains(",")) {
+            String[] items = itemStr.split(",");
+            for (String item : items) {
+                groceryList.add(item.trim());
+            }
+        }
+        else {
+            groceryList.add(itemStr);
+        }
+        return groceryList;
     }
 }
